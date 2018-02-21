215074266011fb6d20b4259fbb583bdac81c76de
Jolanta Jasiulionyte <jolanta@Jolantas-MacBook-Pro.local>
2018-02-21 14:04:47 +0000

    Creates Migrations file out of running manage.py makemigrations command

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -8,13 +8,13 @@ class Board(models.Model):
 class Topic(models.Model):
     subject      = models.CharField(max_length=100)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board        = models.ForeignKey(Board, related_name='topics')
-    starter      = models.ForeignKey(User, related_name='topics')
+    board        = models.ForeignKey(Board, related_name='topics', on_delete=models.PROTECT)
+    starter      = models.ForeignKey(User, related_name='topics', on_delete=models.PROTECT)
 
 class Post(models.Model):
     message    = models.TextField(max_length=800)
-    topic      = models.ForeignKey(Topic, related_name='posts')
+    topic      = models.ForeignKey(Topic, related_name='posts', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts')
-    updateb_by = models.ForeignKey(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.PROTECT)
+    updateb_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.PROTECT)
